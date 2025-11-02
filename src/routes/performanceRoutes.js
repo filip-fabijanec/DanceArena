678615d9ecf8cc7f0e1d3bd4e97b1f8c9dffb465
Martin Tomišić <martin.tomisic@fer.hr>
2025-11-03 00:56:50 +0100

    Azuriran performanceRoutes u /routes-u

---
+++ src/routes/performanceRoutes.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const router = express.Router();
+const Performance = require("../models/Performance");
+
+router.post("/", async (req, res) => {
+  try {
+    const newPerformance = new Performance(req.body);
+    await newPerformance.save({
+      runValidators: true,
+      validateBeforeSave: true,
+    }); // spremanje u bazu podataka
+    res.status(201).json(newPerformance);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const performances = await Performance.find();
+    res.status(200).json(performances);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedPerformance = await Performance.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+      { runValidators: true, validateBeforeSave: true },
+      { context: "query" }
+    );
+    if (!updatedPerformance) {
+      return res.status(404).json({ error: "Performance not found" });
+    }
+    res.status(200).json(updatedPerformance);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await Performance.findByIdAndDelete(req.params.id);
+    res.status(204).send();
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
