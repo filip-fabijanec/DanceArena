e28ad2e29a9dc5479a261475859a1037a9411525
Martin Tomišić <martin.tomisic@fer.hr>
2025-11-03 17:47:39 +0100

    Azuriran userRoutes.js u /routes-u

--- src/routes/scoreRoutes.js
+++ src/routes/scoreRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Score = require("../models/Performance");
+const Score = require("../models/Score");
 
 router.post("/", async (req, res) => {
   try {
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,15 +1,57 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../db');
+const User = require("../models/User");
 
-router.get('/', async (req, res) => {
-    try {
-        const result = await db.query('SELECT * FROM kategorije ORDER BY id');
-        res.render('home', { kategorije: result.rows });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Database error');
+router.post("/", async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save({
+      runValidators: true,
+      validateBeforeSave: true,
+    }); // spremanje u bazu podataka
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    if (!users) {
+      return res.status(404).json({ error: "No users found" });
     }
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+      { runValidators: true, validateBeforeSave: true },
+      { context: "query" }
+    );
+    if (!updatedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(204).send();
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 module.exports = router;
