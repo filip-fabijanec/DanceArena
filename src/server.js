869a4a2f5a969e62166fe7d2374c9726014f825a
filip-fabijanec <ff55912@fer.hr>
2025-11-10 20:13:24 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const path = require('path');
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 // Routes
 app.use('/users', require('./routes/userRoutes'));
@@ -36,5 +36,5 @@ mongoose.connection.on('error', (err) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
