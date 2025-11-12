a61bcf55d12c2811901d8d6cd5ef98f26a336129
Josip P <josip.petricevic@fer.hr>
2025-11-12 16:41:33 +0100

    Pokušao promjeniti nešto za sudce, ne radi :(

--- src/frontend/src/routes/SudacRoutes.js
+++ src/frontend/src/routes/SudacRoutes.js
@@ -2,40 +2,23 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 
-// Importamo sve stranice za organizatora
-import MojaNatjecanja from '../pages/OrgPages/moja_natjecanja_org';
-import KreirajNatjecanje from '../pages/OrgPages/kreiraj_natjecanje';
-import UpravljanjePrijavama from '../pages/OrgPages/prijave_org';
-import OdabirSudaca from '../pages/OrgPages/odabir_sudaca';
-
 import SudacDashboard from '../pages/SudacPages/SudacDashboard';
 import SudacFolder from '../pages/SudacPages/SudacFolder';
 import OcjenjivanjePage from '../pages/SudacPages/OcjenjivanjePage';
 import OcjenjivanjeKategorija from '../pages/SudacPages/OcjenjivanjeKategorija'; 
 
 
-function OrganizatorRoutes() {
+const SudacRoutes = () => {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Navigate to="/organizator/natjecanja" replace />} />
-        <Route path="/natjecanja" element={<MojaNatjecanja />} />
-        <Route path="/kreiranje-natjecanja" element={<KreirajNatjecanje />} />
-        <Route path="/prijave" element={<UpravljanjePrijavama />} />
-        <Route path="/suci" element={<OdabirSudaca />} />
+        <Route path="/" element={<SudacDashboard />} />
+        <Route path="/moja-natjecanja" element={<SudacFolder />} />
+        <Route path="/ocjenjivanje/:competitionId" element={<OcjenjivanjeKategorija />} />
+        <Route path="/ocjenjeno" element={<OcjenjivanjePage />} />
       </Routes>
     </div>
-  );
-}
-const SudacRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<SudacDashboard />} />
-      <Route path="/moja-natjecanja" element={<SudacFolder />} />
-      <Route path="/ocjenjivanje/:competitionId" element={<OcjenjivanjeKategorija />} />
-      <Route path="/ocjenjeno" element={<OcjenjivanjePage />} />
-    </Routes>
   );
 };
 
