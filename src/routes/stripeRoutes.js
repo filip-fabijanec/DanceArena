07cf547750b12c25a43bc4f85d247b2e5bcb553b
filip-fabijanec <ff55912@fer.hr>
2025-12-18 13:21:21 +0100

    Zaboravio u stripeRoutes

--- src/routes/stripeRoutes.js
+++ src/routes/stripeRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createCheckoutSession } = require("../controllers/stripeController");
+const { createCheckoutSession, createSubscriptionCheckoutSession } = require("../controllers/stripeController");
 
 // POST /api/stripe/create-checkout-session
 router.post("/create-checkout-session", createCheckoutSession);
 
+// POST /api/stripe/create-subscription-checkout-session
 router.post('/create-subscription-checkout-session', createSubscriptionCheckoutSession);
 
 // Webhook je registriran direktno u server.js jer mora biti prije express.json()
